9774866f33b523ac31c5ddd4b868f24b4c5787bb
André Pinela <sheffzor@gmail.com>
2016-10-06 23:49:15 +0100

    Setup a initial unlegacy vendor repo
    * Add fancy git and repo bash completions
    * Create a initial common config
    * Setup Google privacy/legal terms URL's
    * Add Unlegacy Android products
    * [Overlay] Enable fused location with google provider
    * [Overlay] Enable Google Backup Transport Service
    * [Overlay] Add nfc help URL
    
    Change-Id: I528e1d6c28cabf430fe0c40b9b6c324dedaaba76
    Signed-off-by: André Pinela <sheffzor@gmail.com>

---
+++ configs/common.mk
@@ -0,0 +1,30 @@
+# Copyright (C) 2016 Unlegacy Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/unlegacy/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# Google property overides
+PRODUCT_PROPERTY_OVERRIDES += \
+    keyguard.no_require_sim=true \
+    ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
+    ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
+    ro.com.android.wifi-watchlist=GoogleGuest \
+    ro.setupwizard.enterprise_mode=1
+
+# Include overlays
+PRODUCT_PACKAGE_OVERLAYS += \
+    vendor/unlegacy/overlay/common
+
---
+++ vendorsetup.sh
@@ -0,0 +1,8 @@
+# Bash completions
+. vendor/unlegacy/bash_completion/git.bash
+. vendor/unlegacy/bash_completion/repo.bash
+
+# Unlegacy Android devices combos
+add_lunch_combo ua_espresso-userdebug
+add_lunch_combo ua_espresso3g-userdebug
+add_lunch_combo ua_tuna-userdebug
