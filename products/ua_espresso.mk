946c5b05273123e0935e71e42d18ba0766f0a108
André Pinela <sheffzor@gmail.com>
2016-10-06 23:49:15 +0100

    Setup a initial unlegacy vendor repo
    * Add fancy git and repo bash completions
    * Create a initial common config
    * Setup Google privacy/legal terms URL's
    * Add Unlegacy Android products
    * [Overlay] Enable fused location with google provider
    * [Overlay] Enable Google Backup Transport Service
    * [Overlay] Add nfc help URL
    
    Change-Id: I528e1d6c28cabf430fe0c40b9b6c324dedaaba76
    Signed-off-by: André Pinela <sheffzor@gmail.com>

---
+++ products/ua_espresso.mk
@@ -0,0 +1,31 @@
+# Copyright (C) 2016 Unlegacy Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit Unlegacy Android common configuration
+$(call inherit-product, vendor/unlegacy/configs/common.mk)
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/samsung/espresso/aosp_espresso.mk)
+
+PRODUCT_NAME   := ua_espresso
+PRODUCT_DEVICE := espresso
+PRODUCT_BRAND  := samsung
+PRODUCT_MODEL  := Galaxy Tab 2
+PRODUCT_MANUFACTURER := Samsung
+
+# Device build info and fingerprint
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=espresso10wifixx \
+    BUILD_FINGERPRINT="samsung/espresso10wifixx/espresso10wifi:4.2.2/JDQ39/P5110XXDML1:user/release-keys" \
+    PRIVATE_BUILD_DESC="espresso10wifixx-user 4.2.2 JDQ39 P5110XXDML1 release-keys"
---
+++ vendorsetup.sh
@@ -0,0 +1,6 @@
+# Bash completions
+. vendor/unlegacy/bash_completion/git.bash
+. vendor/unlegacy/bash_completion/repo.bash
+
+# Unlegacy Android devices combos
+add_lunch_combo ua_espresso-userdebug
