0b55c1db27878f19b2ef103fabfff6e15e6dd3c6
Andreas Blaesius <skate4life@gmx.de>
2017-01-14 09:13:09 +0000

    Add some more build commands
    - add "bacon" command: synonym for "otapackage"
    - add "breakfast" command: add a lunch combo, else it automatically 
      lunches "ua_$target-userdebug"
    - add "mka" command: synonym for "make", automatically sets jobs for the
      build process
    - add "brunch" command: runs breakfast & mka bacon
    
    Those commands were taken from CM and are used on most custom roms.
    Let's make it easy people joining UA to use theire well known commands
    to build our rom.
    
    Change-Id: Ifd4a33f0436cf9e594373c826a011c8a10d792f5

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,7 @@
-# Bash completions
+# Include our bits
 . vendor/unlegacy/bash_completion/git.bash
 . vendor/unlegacy/bash_completion/repo.bash
+. vendor/unlegacy/envsetup.sh
 
 # Unlegacy Android devices combos
 add_lunch_combo ua_espresso-userdebug
