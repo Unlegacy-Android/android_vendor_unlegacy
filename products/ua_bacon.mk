da14a954d46958b665ba89700e34dbfedbd40b06
André Pinela <sheffzor@gmail.com>
2017-07-05 19:05:46 +0100

    products: Add Oneplus One (bacon)
    Change-Id: I8726fd3eb984305f82b366dabf3567d49c548bd7
    Signed-off-by: André Pinela <sheffzor@gmail.com>

---
+++ products/ua_bacon.mk
@@ -0,0 +1,31 @@
+# Copyright (C) 2017 Unlegacy Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit Unlegacy Android common configuration
+$(call inherit-product, vendor/unlegacy/configs/common.mk)
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/oneplus/bacon/aosp_bacon.mk)
+
+PRODUCT_NAME   := ua_bacon
+PRODUCT_DEVICE := bacon
+PRODUCT_BRAND := oneplus
+PRODUCT_MODEL := A0001
+PRODUCT_MANUFACTURER := oneplus
+
+# Device build info
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bacon \
+    BUILD_FINGERPRINT=oneplus/bacon/A0001:6.0.1/MHC19Q/ZNH2KAS1KN:user/release-keys \
+    PRIVATE_BUILD_DESC="bacon-user 6.0.1 MHC19Q ZNH2KAS1KN release-keys"
