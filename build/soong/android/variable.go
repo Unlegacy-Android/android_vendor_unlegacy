fb308aee33fe2dfdbf3e1f1b1bf07ce6ed568baa
Adrian DC <radian.dc@gmail.com>
2017-07-24 14:05:30 +0200

    [2/2] soong_config: Add TARGET_NEEDS_PLATFORM_TEXTRELS
    * Soong product variable 'needs_platform_textrels'
    
    Change-Id: Ia22c94922f37c49f0d66a67747efa0ee97b4e477
    Signed-off-by: Adrian DC <radian.dc@gmail.com>
    Signed-off-by: André Pinela <sheffzor@gmail.com>

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,6 +1,10 @@
 package android
 type Product_variables struct {
+	Needs_platform_textrels struct {
+		Cppflags []string
+	}
 }
 
 type ProductVariables struct {
+	Needs_platform_textrels  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -2,4 +2,5 @@ unlegacy_soong:
 	$(hide) mkdir -p $(dir $@)
 	$(hide) (\
 	echo '{'; \
+	echo '    "Needs_platform_textrels": $(if $(filter true,$(TARGET_NEEDS_PLATFORM_TEXTRELS)),true,false),'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
