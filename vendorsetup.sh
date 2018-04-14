d6607c62059ff593d2eb448e436bb67acf833014
Andreas Blaesius <andi@unlegacy-android.org>
2018-04-14 17:27:02 +0200

    [HACK] Patch external/svox
    The svox repo can't be forked due to the following:
    https://github.com/github/dmca/blob/master/2014/2014-12-22-Cambridge-Mobile.md
    
    On Android 7 we use cee78199bbfae81f54a40671db47096f5f32cda as
    revision ( https://android.googlesource.com/platform/external/svox/+/cee78199bbfae81f54a40671db47096f5f32cda )
    which does not work on Android 6 and below.
    
    Greps through the git log to determine if the specific Bug-ID
    has been fixed already. If it isn't, it applies the patch using
    git am.
    
    Change-Id: Id312672ec1a93d4e52b0f81ec8134419ca70ead4
    Signed-off-by: Andreas Blaesius <andi@unlegacy-android.org>
    Reviewed-on: https://gerrit.unlegacy-android.org/6381

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,3 +8,15 @@ add_lunch_combo ua_espresso-userdebug
 add_lunch_combo ua_espresso3g-userdebug
 add_lunch_combo ua_picasso2-userdebug
 add_lunch_combo ua_tuna-userdebug
+
+while read -r file; do
+    project="$(dirname ${file} | sed 's|vendor/unlegacy/patch/||g')"
+    bugid="$(grep '^Bug: ' ${file} | sed -E 's|^\s*Bug: ([0-9]+).*$|\1|g')"
+    if [ "$(git -C ${project} log --grep "Bug: ${bugid}")" ]; then
+        echo "${project}: b/${bugid} is already patched"
+        continue
+    fi
+    git -C "${project}" am -q "$(pwd)/${file}" || git -C "${project}" am --abort
+done <<< "$(find vendor/unlegacy/patch -type f)"
+unset project
+unset bugid
