a56d61e016ef0b3ae29aef90e7bf4b1746bac635
Sudokamikaze <sudokamikaze@protonmail.com>
2018-09-08 12:36:52 +0300

    mako: Move BUILD_FINGERPRINT out from PROP_OVERRIDES
    Change-Id: Id4d01b372280fa8d0b6d8ee8e8fad86140f5d0e3
    Signed-off-by: Sudokamikaze <sudokamikaze@protonmail.com>
    Reviewed-on: https://gerrit.unlegacy-android.org/10480
    Reviewed-by: Dániel Járai <jaraidaniel@gmail.com>

--- products/ua_mako.mk
+++ products/ua_mako.mk
@@ -27,5 +27,6 @@ PRODUCT_MANUFACTURER := LGE
 # Device build info and fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=occam \
-    BUILD_FINGERPRINT="google/occam/mako:5.1.1/LMY48T/2237560:user/release-keys" \
     PRIVATE_BUILD_DESC="occam-user 5.1.1 LMY48T 2237560 release-keys"
+
+BUILD_FINGERPRINT := google/occam/mako:5.1.1/LMY48T/2237560:user/release-keys
